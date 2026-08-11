a8d15151b1450d42d8169b188ab6f092019efb86
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2026-08-11 09:27:54 +0530

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -28,3 +28,73 @@ unique(product_name)
 alter table products
 modify column product_name varchar(150);
 
+use employee;
+alter table products rename to product_details;
+
+drop table products;
+
+#Insert
+#creating values in products
+insert into product_details (product_name,product_price,product_stock) values ('samsung a07','25000','8');
+
+select * from product_details;
+
+#alternative method
+insert into employee_details values
+(1,"Anirudh",22,'Software','anirudhtanil85@gmail.com'),
+(2,"Komali",22,'Software','komali@gmail.com');
+
+select * from employee_details;
+
+truncate table employee_details;
+
+create database sample;
+drop database sample;
+
+create table product_info(
+pr_id int auto_increment primary key,
+pr_name varchar(50),
+man_place varchar(50),
+expiry int
+);
+
+insert into product_info(pr_name,man_place,expiry) values
+("samsung","Delhi", 70),
+("OnePlus","Mumbai", 70),
+("Apple","Goa", 70),
+("Google","Kerala", 70);
+
+select * from product_info;
+select pr_name from product_info;
+
+select * from product_info WHERE expiry = 70;
+select pr_name from product_info where man_place = "Mumbai";
+
+#Update
+select * from product_info;
+update product_info set man_place = "Raipur" where pr_id = 1;
+
+#SAFE UPDATE MODE --> it prevents from accidentally delete or update too many rows from a table.
+
+#to disable it 
+set SQL_SAFE_UPDATES=0 ;
+
+#DELETE
+
+delete from product_info WHERE pr_id = 3;
+select * from product_info;
+
+#TCL -- Transaction Control Language
+start transaction;
+#begin;
+
+select * from product_info;
+insert into product_info values (5,"Vivo","Kerala",60);
+savepoint x;
+insert into product_info values (6,"Oppo","Kerala",60);
+select * from product_info;
+
+rollback;
+commit;
+
+delete from product_info where pr_id= 6;
