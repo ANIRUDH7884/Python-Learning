a01ddd45e735d8ee584c77aeeb3229d9bd5ec0c3
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2026-08-12 20:38:02 +0530

    Update functions.py

--- Python/functions.py
+++ Python/functions.py
@@ -31,3 +31,44 @@ def wish_Function(name = "friend") :
 
 print(wish_Function("Anirudh"))
 print(wish_Function())
+
+#exersices
+
+def greeting () :
+    print("Welcome")
+greeting()
+
+#task2
+def greet(name):
+    print("Welcome", name)
+
+greet("Anirudh")
+
+# task3
+
+def sum(a , b) :
+
+    total = a + b
+    print("Total",total)
+
+sum(19,17)
+
+# 👉 Function to find square of a number
+
+def square_num(number):
+
+    square = number * number 
+    return square
+
+result = square_num(10)
+print("Square - ",result)
+
+# Function to check: even or odd
+
+def number_check(number):
+    if number % 2 == 0 :
+        print("This is an Even Number")
+    elif number % 2 == 1:
+        print("This is an odd Number")
+
+number_check(10)
