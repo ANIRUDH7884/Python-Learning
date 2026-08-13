e87aba472b281a8c0e076316c4fcda5f996a5897
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2026-08-13 09:32:39 +0530

    Update operators.sql

--- sql/operators.sql
+++ sql/operators.sql
@@ -37,3 +37,52 @@ select * from job_placement where stream is null;
 
 -- is not null -- 
 select * from job_placement where stream is not null;
+
+-- Like --
+-- special Operators are % and _
+select * from job_placement where college_name like "c%";
+select * from job_placement where name like "j_______";
+select * from job_placement where college_name like "%a";
+
+-- distinct -- to find unique values
+
+select distinct(stream) from job_placement;
+select distinct(college_name) from job_placement;
+
+-- Sql Functions --
+
+-- Number Function
+select abs(gpa) as grade_point from job_placement;
+
+select round(1.8998,2) as result;
+
+select ceiling(1.9872)as result;
+
+select floor(3.902) as result ;
+
+select power(2,3) as power;
+
+select sqrt(100) as square;
+
+select truncate(3.567,2);
+
+select rand();
+
+select 15 % 3 as remainder;
+
+select sign(0) as sign;
+
+-- String Function
+
+select upper(name) , stream,college_name from job_placement;
+select lower(name) , stream,college_name from job_placement;
+
+select length("Anirudh T Anil") as length;
+select trim("Anirudh T Anil") as result;
+
+select concat("hello"," Anirudh") as result;
+
+select replace('I like java','like','love') as reult;
+
+select ltrim('  ANIRUDH') AS RESULT;
+select rtrim('Anirudh   ')as Coki;
