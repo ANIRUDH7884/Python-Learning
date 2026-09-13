97dd2bdeed108a2f8dfa9f66e88f845ac9e78a24
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2026-09-13 19:50:41 +0530

    pandas

---
+++ Machine Learning/Pandas_test.py
@@ -0,0 +1,36 @@
+#pandas Library using to clean the messy data / analyze the data
+#comonly used for Data analysis 
+#pandas - Python Data Analysis / Panel Data
+
+import pandas as pd
+
+#series - A pandas Series is like a Column in a Table
+
+a = [1, 2, 3]
+
+myVar = pd.Series(a)
+
+print(myVar)
+
+#label - Changing the name of Index
+arr = ["Anirudh", "Ammu", "Kannan", "komali"]
+
+array = pd.Series(arr , index = ["Name" , "name", "name", "name"])
+
+print(array)
+print(array["name"])
+
+#DataFrame - ITS A 2D DATA STRUCTURE
+
+data = {
+    "Calories" : [420 , 560, 789],
+    "Duration" : [50 , 40, 50]
+ }
+
+df = pd.DataFrame(data)
+print(df)
+
+#Locate Row
+print(df.loc[0])
+print(df.loc[[0 , 1,2]])
+
